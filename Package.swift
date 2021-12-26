fdbfa0c38953aae6c8ba3e3e367bb5711e431acb
Austin Welch <AustinWelch276@icloud.com>
2021-12-26 17:34:39 -0500

    Why.

--- Package.swift
+++ Package.swift
@@ -8,24 +8,18 @@ let package = Package(
     ],
     products: [
         .executable(name: "server-app", targets: ["App"]),
-        .executable(name: "server-client", targets: ["Client"])
+        .executable(name: "server-client", targets: ["Client"]),
     ],
     dependencies: [
         // 💧 A server-side Swift web framework.
         .package(url: "https://github.com/vapor/vapor.git", from: "4.0.0"),
-        .package(name: "Tokamak", url: "https://github.com/TokamakUI/Tokamak", from: "0.6.0"),
+        .package(name: "Tokamak", url: "https://github.com/TokamakUI/Tokamak", from: "0.9.0"),
     ],
     targets: [
         .target(
             name: "App",
             dependencies: [
                 .product(name: "Vapor", package: "vapor")
-            ],
-            swiftSettings: [
-                // Enable better optimizations when building in Release configuration. Despite the use of
-                // the `.unsafeFlags` construct required by SwiftPM, this flag is recommended for Release
-                // builds. See <https://github.com/swift-server/guides/blob/main/docs/building.md#building-for-production> for details.
-                .unsafeFlags(["-cross-module-optimization"], .when(configuration: .release))
             ]
         ),
         .target(
