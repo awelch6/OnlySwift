f7d09077fb346bd142e90e0ed8ba0c13dce2629e
Austin Welch <AustinWelch276@icloud.com>
2021-12-26 18:05:12 -0500

    Attempt 12

--- Sources/App/routes.swift
+++ Sources/App/routes.swift
@@ -1,7 +1,7 @@
 import Vapor
 
 func routes(_ app: Application) throws {
-    try app.register(collection: HomeController())
+//    try app.register(collection: HomeController())
 
     app.get("hello") { req -> String in
         return "Hello, world!"
